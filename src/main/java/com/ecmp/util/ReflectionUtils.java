fb065fc0ca1db5728c7e299823a69b7da6f9efa6
马超 <chao2.ma@changhong.com>
2017-09-11 09:06:31 +0800

    反射工具类优化

--- src/main/java/com/ecmp/util/ReflectionUtils.java
+++ src/main/java/com/ecmp/util/ReflectionUtils.java
@@ -1,139 +1,178 @@
 package com.ecmp.util;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 /**
- * <strong>实现功能:</strong>.
- * <p>反射工具类</p>
- *
- * @author 马超(Vision.Mac)
- * @version 1.0.1 2017/03/06 21:33
+ * 反射的Utils函数集合. 提供访问私有变量,获取泛型类型Class,提取集合中元素的属性等Utils函数.
  */
-public class ReflectionUtils {
+public final class ReflectionUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtils.class);
 
-    private static final Logger logger = LoggerFactory.getLogger(ReflectionUtils.class);
-
-    /**
-     * <p>
-     * 反射 method 方法名，例如 getId
-     * </p>
-     *
-     * @param str 属性字符串内容
-     * @return 返回get方法名
-     */
-    public static String getMethodCapitalize(final String str) {
-        return "get" + StringUtils.capitalize(str);
+    private ReflectionUtils() {
     }
 
     /**
-     * 获取 public get方法的值
-     *
-     * @param cls class
-     * @param entity 实体
-     * @param str    属性字符串内容
-     * @return Object
+     * 直接读取对象属性值,无视private/protected修饰符,不经过getter函数.
      */
-    public static Object getMethodValue(Class<?> cls, Object entity, String str) {
-        Object obj = null;
+    public static Object getFieldValue(final Object object, final String fieldName) {
+        Field field = getDeclaredField(object, fieldName);
+
+        if (field == null) {
+            throw new IllegalArgumentException("Could not find field [" + fieldName + "] on target [" + object + "]");
+        }
+
+        makeAccessible(field);
+
+        Object result = null;
         try {
-            Method method = cls.getMethod(getMethodCapitalize(str));
-            obj = method.invoke(entity);
-        } catch (NoSuchMethodException e) {
-            logger.error(String.format("Warn: No such method. in %s.  Cause:", cls.getSimpleName()) + e);
+            result = field.get(object);
         } catch (IllegalAccessException e) {
-            logger.error(String.format("Warn: Cannot execute a private method. in %s.  Cause:", cls.getSimpleName()) + e);
-        } catch (InvocationTargetException e) {
-            logger.error("Warn: Unexpected exception on getMethodValue.  Cause:" + e);
+            LOGGER.error("不可能抛出的异常{}", e.getMessage());
         }
-        return obj;
+        return result;
     }
 
     /**
-     * 获取 public get方法的值
-     *
-     * @param entity 实体
-     * @param str    属性字符串内容
-     * @return Object
+     * 直接设置对象属性值,无视private/protected修饰符,不经过setter函数.
      */
-    public static Object getMethodValue(Object entity, String str) {
-        if (null == entity) {
-            return null;
+    public static void setFieldValue(final Object object, final String fieldName, final Object value) {
+        Field field = getDeclaredField(object, fieldName);
+
+        if (field == null) {
+            throw new IllegalArgumentException("Could not find field [" + fieldName + "] on target [" + object + "]");
         }
-        return getMethodValue(entity.getClass(), entity, str);
+
+        makeAccessible(field);
+
+        try {
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("不可能抛出的异常:{}", e.getMessage());
+        }
     }
 
     /**
-     * 通过反射, 获得Class定义中声明的泛型参数的类型, 注意泛型必须定义在父类处
-     * 如无法找到, 返回Object.class.
-     * eg.
-     * public UserDao extends HibernateDao&lt;User&gt;
-     *
-     * @param <T>   泛型
-     * @param clazz The class to introspect
-     * @return the first generic declaration, or Object.class if cannot be determined
+     * 循环向上转型,获取对象的DeclaredField.
+     */
+    protected static Field getDeclaredField(final Object object, final String fieldName) {
+        if (Objects.isNull(object)) {
+            throw new IllegalArgumentException("object不能为空");
+        }
+        return getDeclaredField(object.getClass(), fieldName);
+    }
+
+    /**
+     * 循环向上转型,获取类的DeclaredField.
      */
     @SuppressWarnings("unchecked")
-    public static <T> Class<T> getClassGenricType(final Class clazz) {
+    protected static Field getDeclaredField(final Class clazz, final String fieldName) {
+        if (Objects.isNull(clazz)) {
+            throw new IllegalArgumentException("clazz不能为空");
+        }
+        if (Objects.isNull(fieldName)) {
+            throw new IllegalArgumentException("fieldName不能为空");
+        }
+        for (Class superClass = clazz; superClass != Object.class; superClass = superClass.getSuperclass()) {
+            try {
+                return superClass.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                // Field不在当前类定义,继续向上转型
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 强制转换fileld可访问.
+     */
+    protected static void makeAccessible(final Field field) {
+        if (!Modifier.isPublic(field.getModifiers()) || !Modifier.isPublic(field.getDeclaringClass().getModifiers())) {
+            field.setAccessible(true);
+        }
+    }
+
+    /**
+     * 通过反射,获得定义Class时声明的父类的泛型参数的类型. 如public UserDao extends HibernateDao<User>
+     *
+     * @param clazz The class to introspect
+     * @return the first generic declaration, or Object.class if cannot be
+     * determined
+     */
+    @SuppressWarnings("unchecked")
+    public static Class getSuperClassGenricType(final Class clazz) {
         return getSuperClassGenricType(clazz, 0);
     }
 
     /**
-     * 通过反射, 获得Class定义中声明的父类的泛型参数的类型.
-     * 如无法找到, 返回Object.class.
-     * 如public UserDao extends HibernateDao&lt;User,Long&gt;
+     * 通过反射,获得定义Class时声明的父类的泛型参数的类型. 如public UserDao extends
+     * HibernateDao<User,Long>
      *
      * @param clazz clazz The class to introspect
      * @param index the Index of the generic ddeclaration,start from 0.
-     * @return the index generic declaration, or Object.class if cannot be determined
+     * @return the index generic declaration, or Object.class if cannot be
+     * determined
      */
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings("unchecked")
     public static Class getSuperClassGenricType(final Class clazz, final int index) {
-
         Type genType = clazz.getGenericSuperclass();
 
         if (!(genType instanceof ParameterizedType)) {
-            logger.error(String.format("Warn: %s's superclass not ParameterizedType", clazz.getSimpleName()));
+            LOGGER.warn(clazz.getSimpleName() + "'s superclass not ParameterizedType");
             return Object.class;
         }
 
         Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
 
         if (index >= params.length || index < 0) {
-            logger.error(String.format("Warn: Index: %s, Size of %s's Parameterized Type: %s .", index, clazz.getSimpleName(),
-                    params.length));
+            LOGGER.warn("Index: " + index + ", Size of " + clazz.getSimpleName() + "'s Parameterized Type: " + params.length);
             return Object.class;
         }
         if (!(params[index] instanceof Class)) {
-            logger.error(String.format("Warn: %s not set the actual class on superclass generic parameter",
-                    clazz.getSimpleName()));
+            LOGGER.warn(clazz.getSimpleName() + " not set the actual class on superclass generic parameter");
             return Object.class;
         }
-
         return (Class) params[index];
     }
 
     /**
-     * 将反射时的checked exception转换为unchecked exception.
+     * 提取集合中的对象的属性,组合成List.
      *
-     * @param e checked exception
-     * @return unchecked exception
+     * @param collection   来源集合.
+     * @param propertyName 要提取的属性名.
      */
-    public static RuntimeException convertReflectionExceptionToUnchecked(Exception e) {
-        if (e instanceof IllegalAccessException || e instanceof IllegalArgumentException
-                || e instanceof NoSuchMethodException) {
-            return new IllegalArgumentException("Reflection Exception.", e);
-        } else if (e instanceof InvocationTargetException) {
-            return new RuntimeException("Reflection Exception.", ((InvocationTargetException) e).getTargetException());
-        } else if (e instanceof RuntimeException) {
-            return (RuntimeException) e;
+    @SuppressWarnings("unchecked")
+    public static List fetchElementPropertyToList(final Collection collection, final String propertyName) throws Exception {
+        List list = new ArrayList();
+        for (Object obj : collection) {
+            list.add(PropertyUtils.getProperty(obj, propertyName));
         }
-        return new RuntimeException("Unexpected Checked Exception.", e);
+
+        return list;
+    }
+
+    /**
+     * 提取集合中的对象的属性,组合成由分割符分隔的字符串.
+     *
+     * @param collection   来源集合.
+     * @param propertyName 要提取的属性名.
+     * @param separator    分隔符.
+     */
+    @SuppressWarnings("unchecked")
+    public static String fetchElementPropertyToString(final Collection collection, final String propertyName, final String separator)
+            throws Exception {
+        List list = fetchElementPropertyToList(collection, propertyName);
+        return StringUtils.join(list.toArray(), separator);
     }
 }
