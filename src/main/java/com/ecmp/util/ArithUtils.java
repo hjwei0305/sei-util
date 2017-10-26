22c368dd372965b3baadc0f1da87f7a3dab73cfa
马超 <chao2.ma@changhong.com>
2017-10-26 20:28:59 +0800

    调整优化

--- src/main/java/com/ecmp/util/ArithUtils.java
+++ src/main/java/com/ecmp/util/ArithUtils.java
@@ -33,6 +33,18 @@ public class ArithUtils {
     }
 
     /**
+     * 提供精确加法计算的add方法
+     *
+     * @param value1 被加数
+     * @param value2 加数
+     * @return 两个参数的和
+     */
+    public static BigDecimal add(BigDecimal value1, double value2) {
+        BigDecimal b2 = new BigDecimal(value2);
+        return value1.add(b2);
+    }
+
+    /**
      * 提供精确减法运算的sub方法
      *
      * @param value1 被减数
@@ -46,6 +58,18 @@ public class ArithUtils {
     }
 
     /**
+     * 提供精确减法运算的sub方法
+     *
+     * @param value1 被减数
+     * @param value2 减数
+     * @return 两个参数的差
+     */
+    public static BigDecimal sub(BigDecimal value1, double value2) {
+        BigDecimal b2 = new BigDecimal(value2);
+        return value1.subtract(b2);
+    }
+
+    /**
      * 提供精确乘法运算的mul方法
      *
      * @param value1 被乘数
@@ -56,6 +80,18 @@ public class ArithUtils {
         BigDecimal b1 = new BigDecimal(value1);
         BigDecimal b2 = new BigDecimal(value2);
         return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确乘法运算的mul方法
+     *
+     * @param value1 被乘数
+     * @param value2 乘数
+     * @return 两个参数的积
+     */
+    public static BigDecimal mul(BigDecimal value1, double value2) {
+        BigDecimal b2 = new BigDecimal(value2);
+        return value1.multiply(b2);
     }
 
     /**
