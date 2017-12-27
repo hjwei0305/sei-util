6065996dfe85c6b89a200623ff6a1f5c53e8fd22
马超 <chao2.ma@changhong.com>
2017-12-27 16:29:35 +0800

    remark增加排序

--- src/main/java/com/ecmp/annotation/Remark.java
+++ src/main/java/com/ecmp/annotation/Remark.java
@@ -17,7 +17,10 @@ public @interface Remark {
      * @return 简要注解说明
      */
     String value();
-
+    /**
+     * @return 排序
+     */
+    int rank() default 0;
     /**
      * @return 注释说明：用于描述代码内部用法说明，一般不用于业务
      */
