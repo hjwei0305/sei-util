01ed44f7a8c08f582ae899eeb527a2fb429adec4
马超 <chao2.ma@changhong.com>
2018-03-12 15:31:57 +0800

    增加用户类型枚举

---
+++ src/main/java/com/ecmp/enums/UserType.java
@@ -0,0 +1,35 @@
+package com.ecmp.enums;
+
+import com.ecmp.annotation.Remark;
+
+/**
+ * 实现功能：用户类型：枚举
+ *
+ * @author 高银军
+ * @version 1.0.00      2017/4/18 11:08
+ */
+public enum UserType {
+    /**
+     * 员工
+     */
+    @Remark("员工")
+    Employee,
+
+    /**
+     * 供应商
+     */
+    @Remark("供应商")
+    Supplier,
+
+    /**
+     * 客户
+     */
+    @Remark("客户")
+    Customer,
+
+    /**
+     * 专家
+     */
+    @Remark("专家")
+    Expert;
+}
