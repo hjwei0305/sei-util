d74d6efbee3da35e8fe734a31bd7fb4508244dda
Vision.Mac <chao2.ma@changhong.com>
2020-01-23 15:09:38 +0800

    优化线程变量工具类

--- src/main/java/com/chonghong/sei/util/thread/ThreadLocalUtil.java
+++ src/main/java/com/chonghong/sei/util/thread/ThreadLocalUtil.java
@@ -46,21 +46,21 @@ public final class ThreadLocalUtil {
      * 读可传播的线程变量
      */
     public static String getTranVar(String key) {
-        return get(ThreadLocalHolder.TRAN_VAR, key.toLowerCase());
+        return get(ThreadLocalHolder.TRAN_VAR, key.toUpperCase());
     }
 
     /**
      * 写可传播的线程变量
      */
     public static void setTranVar(String key, String value) {
-        set(ThreadLocalHolder.TRAN_VAR, key.toLowerCase(), value);
+        set(ThreadLocalHolder.TRAN_VAR, key.toUpperCase(), value);
     }
 
     /**
      * 删可传播的线程变量
      */
     public static void removeTranVar(String key) {
-        remove(ThreadLocalHolder.TRAN_VAR, key.toLowerCase());
+        remove(ThreadLocalHolder.TRAN_VAR, key.toUpperCase());
     }
 
     @SuppressWarnings("unchecked")
