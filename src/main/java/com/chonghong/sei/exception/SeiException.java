1fd7339083b9e69bcf3f175ff6e6100b14c866be
王锦光 <wangjg@changhong.com>
2020-02-04 17:41:49 +0800

    删除异常类manager,VERSION = 6.0.1.2

--- src/main/java/com/chonghong/sei/exception/SeiException.java
+++ src/main/java/com/chonghong/sei/exception/SeiException.java
@@ -2,20 +2,20 @@ package com.chonghong.sei.exception;
 
 /**
  * <strong>实现功能:</strong>.
- * <p>ECMP平台异常基类</p>
+ * <p>平台异常基类</p>
  *
  * @author 马超(Vision.Mac)
  * @version 1.0.1 2017/2/8 10:20
  */
-public class EcmpException extends BaseRuntimeException {
+public class SeiException extends BaseRuntimeException {
 
     private static final long serialVersionUID = -2896111728089420354L;
 
-    public EcmpException(String msg) {
+    public SeiException(String msg) {
         super(msg);
     }
 
-    public EcmpException(String msg, Throwable cause) {
+    public SeiException(String msg, Throwable cause) {
         super(msg, cause);
     }
 }
